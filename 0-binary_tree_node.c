90b9d7f401f99387428ccbade7175def30f24a10
Khaled Empeshy <empeshy20036969@gmail.com>
2024-04-12 16:12:48 +0200

    2nd 3rd 4th

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -9,12 +9,15 @@
  *
  * Return: pointer to the new node or NULL in failling
  */
-binary_tree_t *binary_tree_node(binary_tree_t * parent, int value)
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 		binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
 
 		if (!new_node)
+		{
 				return (NULL);
+		}
+
 		new_node->n = value;
 		new_node->parent = parent;
 		new_node->left = NULL;
